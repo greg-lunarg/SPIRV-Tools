60de6adb0c3adcd3810f3a9d2a411624b7b07fe4
Greg Fischer <greg@lunarg.com>
2016-12-19 19:00:52 -0700

    SSAMem: Code, First Pass

--- source/opt/ssamem_pass.cpp
+++ source/opt/ssamem_pass.cpp
@@ -17,28 +17,207 @@
 #include "ssamem_pass.h"
 #include "iterator.h"
 
-#include <unordered_map>
-
-#define SPV_ENTRY_POINT_FUNCTION_ID 1
+/*
 #define SPV_FUNCTION_CALL_FUNCTION_ID 2
 #define SPV_FUNCTION_CALL_ARGUMENT_ID 3
 #define SPV_FUNCTION_PARAMETER_RESULT_ID 1
+#define SPV_STORE_POINTER_ID 0
 #define SPV_STORE_OBJECT_ID 1
 #define SPV_RETURN_VALUE_ID 0
 #define SPV_TYPE_POINTER_STORAGE_CLASS 1
 #define SPV_TYPE_POINTER_TYPE_ID 2
+*/
+
+static const int SPV_ENTRY_POINT_FUNCTION_ID = 1;
+static const int SPV_STORE_POINTER_ID = 0;
+static const int SPV_STORE_VAL_ID = 1;
+static const int SPV_ACCESS_CHAIN_PTR_ID = 0;
+static const int SPV_ACCESS_CHAIN_IDX0_ID = 1;
+static const int SPV_TYPE_PTR_STORAGE_CLASS = 0;
+static const int SPV_TYPE_PTR_TYPE_ID = 1;
+static const int SPV_LOAD_PTR_ID = 0;
 
 namespace spvtools {
 namespace opt {
 
-bool SSAMemPass::SSAMem(ir::Function* func) {
+bool SSAMemPass::isMathType(ir::Instruction* typeInst) {
+  switch (typeInst->opcode()) {
+  case SpvOpTypeInt:
+  case SpvOpTypeFloat:
+  case SpvOpTypeBool:
+  case SpvOpTypeVector:
+  case SpvOpTypeMatrix:
+    return true;
+  default:
+    break;
+  }
+  return false;
+}
+
+bool SSAMemPass::isTargetType(ir::Instruction* typeInst) {
+  if (isMathType(typeInst))
+    return true;
+  if (typeInst->opcode() != SpvOpTypeStruct)
+    return false;
+  int nonMathComp = 0;
+  typeInst->ForEachInId([&nonMathComp,this](uint32_t* tid) {
+    ir::Instruction* typeInst =
+        def_use_mgr_->id_to_defs().find(*tid)->second;
+    if (!isMathType(typeInst)) nonMathComp++;
+  });
+  return nonMathComp == 0;
+}
+
+void SSAMemPass::SSAMemAnalyze(ir::Function* func) {
+  ssaVars.clear();
+  ssaComps.clear();
+  ssaCompVars.clear();
+  nonSsaVars.clear();
+  for (auto bi = func->begin(); bi != func->end(); bi++) {
+    for (auto ii = bi->begin(); ii != bi->end(); ii++) {
+      if (ii->opcode() != SpvOpStore)
+        continue;
+      const uint32_t ptrId = ii->GetInOperand(SPV_STORE_POINTER_ID).words[0];
+      const ir::Instruction* ptrInst =
+          def_use_mgr_->id_to_defs().find(ptrId)->second;
+      const uint32_t varId = ptrInst->opcode() == SpvOpAccessChain ?
+          ptrInst->GetInOperand(SPV_ACCESS_CHAIN_PTR_ID).words[0] :
+          ptrId;
+
+      // Verify store variable is target type
+      const ir::Instruction* varInst =
+          def_use_mgr_->id_to_defs().find(varId)->second;
+      const uint32_t varTypeId = varInst->type_id();
+      if (seenNonTargetTypes.find(varTypeId) != seenNonTargetTypes.end()) {
+        nonSsaVars.insert(varId);
+        continue;
+      }
+      if (seenTargetTypes.find(varTypeId) == seenTargetTypes.end()) {
+        const ir::Instruction* varTypeInst =
+            def_use_mgr_->id_to_defs().find(varTypeId)->second;
+        if (varTypeInst->GetInOperand(SPV_TYPE_PTR_STORAGE_CLASS).words[0] !=
+            SpvStorageClassFunction) {
+          seenNonTargetTypes.insert(varTypeId);
+          nonSsaVars.insert(varId);
+          continue;
+        }
+        const uint32_t varPteTypeId =
+            varTypeInst->GetInOperand(SPV_TYPE_PTR_TYPE_ID).words[0];
+        ir::Instruction* varPteTypeInst =
+            def_use_mgr_->id_to_defs().find(varPteTypeId)->second;
+        if (!isTargetType(varPteTypeInst)) {
+          seenNonTargetTypes.insert(varTypeId);
+          nonSsaVars.insert(varId);
+          continue;
+        }
+        if (ptrInst->opcode() == SpvOpAccessChain &&
+            isMathType(varPteTypeInst)) {
+          seenNonTargetTypes.insert(varTypeId);
+          nonSsaVars.insert(varId);
+          continue;
+        }
+        seenTargetTypes.insert(varTypeId);
+      }
+
+      // Verify store variable/component is not yet assigned.
+      // If whole variable is stored, there should not be any
+      // component stores and vice-versa.
+      if (nonSsaVars.find(varId) != nonSsaVars.end())
+        continue;
+      if (ssaVars.find(varId) != ssaVars.end()) {
+        nonSsaVars.insert(varId);
+        continue;
+      }
+      if (ptrInst->opcode() == SpvOpAccessChain) {
+        if (ptrInst->NumInOperands() != 2) {
+          nonSsaVars.insert(varId);
+          continue;
+        }
+        const uint32_t idxId =
+            ptrInst->GetInOperand(SPV_ACCESS_CHAIN_IDX0_ID).words[0];
+        if (ssaComps.find(std::make_pair(varId, idxId)) != ssaComps.end()) {
+          nonSsaVars.insert(varId);
+          continue;
+        }
+        ssaCompVars.insert(varId);
+        ssaComps[std::make_pair(varId, idxId)] = &*ii;
+      }
+      else {
+        if (ssaCompVars.find(varId) != ssaCompVars.end()) {
+          nonSsaVars.insert(varId);
+          continue;
+        }
+        ssaVars[varId] = &*ii;
+      }
+    }
+  }
+}
+
+bool SSAMemPass::SSAMemProcess(ir::Function* func) {
   bool modified = false;
   for (auto bi = func->begin(); bi != func->end(); bi++) {
-    for (auto ii = bi->begin(); ii != bi->end();) {
-      ii++;
+    for (auto ii = bi->begin(); ii != bi->end(); ii++) {
+      if (ii->opcode() != SpvOpLoad)
+        continue;
+      const uint32_t ptrId = ii->GetInOperand(SPV_STORE_POINTER_ID).words[0];
+      const ir::Instruction* ptrInst =
+        def_use_mgr_->id_to_defs().find(ptrId)->second;
+      const uint32_t varId = ptrInst->opcode() == SpvOpAccessChain ?
+        ptrInst->GetInOperand(SPV_ACCESS_CHAIN_PTR_ID).words[0] :
+        ptrId;
+      if (nonSsaVars.find(varId) != nonSsaVars.end())
+        continue;
+      uint32_t replId;
+      if (ptrInst->opcode() == SpvOpAccessChain) {
+        // process component load
+        const uint32_t idxId =
+          ptrInst->GetInOperand(SPV_ACCESS_CHAIN_IDX0_ID).words[0];
+        const auto csi = ssaComps.find(std::make_pair(varId, idxId));
+        if (csi != ssaComps.end())
+          replId = csi->second->GetInOperand(SPV_STORE_VAL_ID).words[0];
+        else {
+          // See if the whole variable was stored with a load.
+          // If so, look for a component store into the load variable
+          // and use the value Id that was stored.
+          const auto vsi = ssaVars.find(varId);
+          if (vsi == ssaVars.end())
+            continue;
+          const uint32_t valId =
+              vsi->second->GetInOperand(SPV_STORE_VAL_ID).words[0];
+          const ir::Instruction* valInst =
+              def_use_mgr_->id_to_defs().find(valId)->second;
+          if (valInst->opcode() != SpvOpLoad)
+            continue;
+          const uint32_t loadVarId =
+              valInst->GetInOperand(SPV_LOAD_PTR_ID).words[0];
+          const auto lvcsi = ssaComps.find(std::make_pair(loadVarId, idxId));
+          if (lvcsi == ssaComps.end())
+            continue;
+          replId = lvcsi->second->GetInOperand(SPV_STORE_VAL_ID).words[0];
+        }
+      }
+      else {
+        // process whole variable load
+        const auto vsi = ssaVars.find(varId);
+        // if variable is not defined with whole variable store,
+        // skip this load
+        if (vsi == ssaVars.end())
+          continue;
+        replId = vsi->second->GetInOperand(SPV_STORE_VAL_ID).words[0];
+      }
+      const uint32_t loadId = ii->result_id();
+      const bool replaced = def_use_mgr_->ReplaceAllUsesWith(loadId, replId);
+      if (replaced)
+        modified = true;
     }
   }
   return modified;
+}
+
+bool SSAMemPass::SSAMem(ir::Function* func) {
+    SSAMemAnalyze(func);
+    bool modified = SSAMemProcess(func);
+    return modified;
 }
 
 void SSAMemPass::Initialize(ir::Module* module) {
@@ -53,7 +232,7 @@ void SSAMemPass::Initialize(ir::Module* module) {
 
   module_ = module;
 
-  // initialize function and block maps
+  // Initialize function and block maps
   id2function.clear();
   id2block.clear();
   for (auto& fn : *module_) {
@@ -62,6 +241,10 @@ void SSAMemPass::Initialize(ir::Module* module) {
       id2block[blk.GetLabelId()] = &blk;
     }
   }
+
+  // Initialize Target Type Caches
+  seenTargetTypes.clear();
+  seenNonTargetTypes.clear();
 };
 
 Pass::Status SSAMemPass::ProcessImpl() {
--- source/opt/ssamem_pass.h
+++ source/opt/ssamem_pass.h
@@ -17,7 +17,12 @@
 #ifndef LIBSPIRV_OPT_SSAMEM_PASS_H_
 #define LIBSPIRV_OPT_SSAMEM_PASS_H_
 
+
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
 #include <algorithm>
+#include <utility>
 
 #include "def_use_manager.h"
 #include "module.h"
@@ -41,6 +46,43 @@ class SSAMemPass : public Pass {
   // Map from block's label id to block
   std::unordered_map<uint32_t, ir::BasicBlock*> id2block;
 
+  // Map from SSA Variable to its single store
+  std::unordered_map<uint32_t, ir::Instruction*> ssaVars;
+
+  // Hash for CompKey
+  struct pairhash {
+    public:
+      template <typename T, typename U>
+      std::size_t operator()(const std::pair<T, U> &x) const
+      {
+          return (std::hash<T>()(x.first) << 8) ^ std::hash<U>()(x.second);
+      }
+  };
+
+  // Map from SSA Component (Var, Index pair) to its single store
+  typedef std::pair<uint32_t, uint32_t> CompKey;
+  std::unordered_map<CompKey, ir::Instruction*, pairhash> ssaComps;
+
+  // Set of SSA Component Variables
+  std::unordered_set<uint32_t> ssaCompVars;
+
+  // Set of non-SSA Variables
+  std::unordered_set<uint32_t> nonSsaVars;
+
+  // Set of verified target types
+  std::unordered_set<uint32_t> seenTargetTypes;
+
+  // Set of verified target types
+  std::unordered_set<uint32_t> seenNonTargetTypes;
+
+  // Returns true if type is a scalar type
+  // or a vector or matrix
+  bool isMathType(ir::Instruction* typeInst);
+
+  // Returns true if type is a scalar, vector, matrix
+  // or struct of only those types
+  bool isTargetType(ir::Instruction* typeInst);
+
   // Next unused ID
   uint32_t nextId_;
 
@@ -49,8 +91,23 @@ class SSAMemPass : public Pass {
   }
   inline uint32_t getNextId() { return nextId_++; }
 
-  // Exhaustively inline all function calls in func as well as in
-  // all code that is inlined into func.
+  // Find all function scope variables that are stored to only once
+  // and create two maps: one for full variable stores and one for
+  // component stores. They will map variable (and component index)
+  // to the store value Id. Also cache all variables that
+  // are not SSA. Only analyze variables of scalar, vector, 
+  // matrix types and struct types containing only these types.
+  void SSAMemAnalyze(ir::Function* func);
+
+  // For each load of SSA variable, replace all uses of the load
+  // with the value stored, if possible. Assumes that SSAMemAnalyze
+  // has just been run for func. Return true if the any
+  // instructions are modified.
+  bool SSAMemProcess(ir::Function* func);
+
+  // For each load of SSA variable, replace all uses of the load
+  // with the value stored, if possible. Return true if the any
+  // instructions are modified. 
   bool SSAMem(ir::Function* func);
 
   void Initialize(ir::Module* module);
