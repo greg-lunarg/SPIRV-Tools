31008d19e2d4e84d6bdf1275fc5197dc7a8b1aa7
Greg Fischer <greg@lunarg.com>
2018-11-09 14:18:45 -0700

    Fix header include

--- source/opt/process_lines_pass.cpp
+++ source/opt/process_lines_pass.cpp
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "source/opt/prop_lines_pass.h"
+#include "source/opt/process_lines_pass.h"
 
 #include <set>
 #include <unordered_set>
